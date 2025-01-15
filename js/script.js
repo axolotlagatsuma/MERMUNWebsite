880d5911d21c83b99d8f4e702f405a3c7e5987df
Mükremin Yağız Yaşar <agatsuma@axolotldev.xyz>
2025-01-15 19:55:42 +0300

    aaa
    aaaaa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-document.getElementById('menuToggle').addEventListener('click', function () {
-    const navLinks = document.getElementById('navLinks');
-    navLinks.classList.toggle('show');
+document.getElementById("menuToggle").addEventListener("click", function () {
+    const navLinks = document.getElementById("navLinks");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block";
+    }
 });
